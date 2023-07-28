c8eb740af5f9b3d8187720516e87ce0c3072cd81
Mane <mane.ghudratyan@gmail.com>
2023-07-28 13:03:19 +0400

    Not to create a dictionary, all the changes were in one dictionary

--- Dictionary/exercise19.py
+++ Dictionary/exercise19.py
@@ -1,18 +1,11 @@
 #Write a Python program to combine two dictionary by adding values for common keys.
 
-d1 = {'a' : 100, 'b' : 200, 'c' : 300};
-d2 = {'a' : 300, 'b' : 200, 'd' : 400};
-d3 = {};
+d1 = {'a': 100, 'b': 200, 'c': 300};
+d2 = {'a': 300, 'b': 200, 'd': 400};
 
-for k,v in d1.items():
-    if k in d3:
-        d3[k] += v;
+for k, v in d2.items():
+    if k in d1:
+        d1[k] += v;
     else:
-        d3[k] = v;
-
-for k,v in d2.items():
-    if k in d3:
-        d3[k] += v;
-    else:
-        d3[k] = v;
-print(d3)
+        d1[k] = v;
+print(d1)
